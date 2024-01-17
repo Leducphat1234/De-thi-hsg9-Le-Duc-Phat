e8988ef26c09aef52d57601c0e5bf836962b8fc3
Leducphat1234 <96918446+Leducphat1234@users.noreply.github.com>
2024-01-17 16:00:22 +0700

    Update subseg.pas

--- hsg9 2018-2019 v2/subseg.pas
+++ hsg9 2018-2019 v2/subseg.pas
@@ -1,8 +1,7 @@
-uses math;
+uses SysUtils;
 var fi, fo: text;
-    i, j, k, _, n: longint;
-    A: array of int64;
-    b, S: int64;
+    _, n, mx, s_cur, i: longint;
+    A: array of longint;
 
 begin
     assign(fi, 'subseg.inp'); reset(fi);
@@ -10,33 +9,15 @@ begin
     readln(fi, n);
     setlength(A, n);
     for _ := 0 to n-1 do read(fi, A[_]);
-    S := 0;
-    b := 0;
+    mx := low(longint);
     for i := 0 to n-1 do
     begin
-        for j := i to n-1 do
-        if a[j] >= 0 then 
-        begin
-            S := S + A[j];
-            writeln('(2)', s, ' ', A[j]);
-        end
-        else
-        begin
-            b := A[j-1]; writeln('(3.1)', b);
-            for k := j to n-1 do
-            begin
-                if A[k] < 0 then
-                    b := b + A[k]
-                else if b < b + A[k] then
-                begin
-                    S := S + b + A[k];
-                end;
-                writeln('(3)', s,' ', b, ' ',k, ' ', A[k]);
-            end;
-        end;
-        writeln('(1)',s);
+        s_cur := s_cur + A[i];
+        if mx < s_cur then mx := s_cur;
+        if s_cur < 0 then s_cur := 0;
     end;
-    
+    writeln(fo, mx);
+
     close(fi);
     close(fo);
 end.
