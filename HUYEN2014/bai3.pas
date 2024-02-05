d1b2ba9b9bf4dc59d144579509aecb24561bddd6
Leducphat1234 <96918446+Leducphat1234@users.noreply.github.com>
2024-02-05 16:47:01 +0700

    Add files via upload

--- HUYEN2014/bai3.pas
+++ HUYEN2014/bai3.pas
@@ -1,1 +1,21 @@
 uses SysUtils;
+var m, n, S: longword;
+function FindWay(m, n: longword): longword;
+begin
+    if (m = 1) or (n = 1) then exit(1);
+    FindWay := FindWay(m, n-1) + FindWay(m-1, n);
+end;
+
+BEGIN
+    assign(input, 'bai3.inp'); reset(input);
+    assign(output, 'bai3.out'); rewrite(output);
+    readln(m, n);
+
+    S := FindWay(succ(m), succ(n));
+
+    writeln(S);
+
+
+    close(input);
+    close(output);
+END.
