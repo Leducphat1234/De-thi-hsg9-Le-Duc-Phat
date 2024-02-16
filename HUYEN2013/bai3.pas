8ac8d823ba67e3384d9f235a3025407c4aa41cf7
Leducphat1234 <96918446+Leducphat1234@users.noreply.github.com>
2024-02-16 14:07:03 +0700

    Improve quicksort algorithm

--- HUYEN2013/bai3.pas
+++ HUYEN2013/bai3.pas
@@ -28,8 +28,12 @@ begin
     j := right-1;
     while true do
     begin
-        while s[i] < s[pivot] do inc(i);
-        while s[j] > s[pivot] do dec(j);
+        while s[i] < s[pivot] do
+            if i > left then inc(i)
+            else break;
+        while s[j] > s[pivot] do
+            if j < right then dec(j)
+            else break;
         if i >= j then break;
         swap(s[i], s[j]);
     end;
